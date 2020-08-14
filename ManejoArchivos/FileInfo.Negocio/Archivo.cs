f5ef94fea6f6903b65497bc9306a3ea5740c9314
jhonathan19906 <47836888+jhonathan19906@users.noreply.github.com>
2020-08-13 20:53:29 -0500

    Programación en paralelo
    Este programa muestra una lo que es programación en paralelo 
    donde muestra una archivo de texto, y los directorios dentro de mi sistema de trabajo 
    creado por Jhonathan Chiliquinga

---
+++ ManejoArchivos/FileInfo.Negocio/Archivo.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+
+namespace FileInfo.Negocio
+{
+    public class Archivo
+    {
+        private string path;
+        
+
+        public Archivo()
+        {
+        }
+
+        public Archivo(string path)
+        {
+            this.path = path;
+        }
+
+        public static void Main()
+        {
+            string path = Path.GetTempFileName();
+            var archivo = new Archivo(path);
+
+            // Create a file to write to.
+            using (StreamWriter sw = archivo.CreateText())
+            {
+                sw.WriteLine("Hello");
+                sw.WriteLine("And");
+                sw.WriteLine("Welcome");
+            }
+
+            // Open the file to read from.
+            using (StreamReader sr = archivo.OpenText())
+            {
+                var s = @"C:\Users\HP\source\repos\ManejoArchivos\Nelson Mandela.txt "; ;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            try
+            {
+                string path2 = Path.GetTempFileName();
+                var fi2 = new Archivo(path2);
+
+              
+              // Asegúrese de que el objetivo no exista
+                fi2.Delete();
+
+                // Copie el archivo.
+                archivo.CopyTo(path2);
+                Console.WriteLine($"{path} was copied to {path2}.");
+
+                // Elimina el archivo recién creado.
+                fi2.Delete();
+                Console.WriteLine($"{path2} was successfully deleted.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"The process failed: {e.ToString()}");
+            }
+        }
+
+        private void CopyTo(string path2)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StreamWriter CreateText()
+        {
+            throw new NotImplementedException();
+        }
+
+        public  StreamReader OpenText()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ManejoArchivos/ManejoArchivos.Consola/Log.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ManejoArchivos.Consola
+{
+    public class Log
+    {
+        private string Path = "";
+        
+        public Log(string Path)
+        {
+            this.Path = Path;
+        }
+        public void Add( string sLog)
+        {
+            CreateDirectory();
+            string nombre = GetNameFile();
+            string cadena = "";
+
+            cadena += DateTime.Now + "_" + sLog + Environment.NewLine;
+
+            StreamWriter sw = new StreamWriter(Path + "/" + nombre, true);
+            sw.Write(cadena);
+            sw.Close();
+        }
+        private string GetNameFile()
+        {
+            string nombre = "";
+            nombre = "log" + DateTime.Now.Year + "_" + DateTime.Now.Month + "_" + DateTime.Now.Day + ".txt";
+
+            return nombre;
+        }
+        public void CreateDirectory()
+        {
+            try
+            {
+                if (!Directory.Exists(Path))
+                    Directory.CreateDirectory(Path);
+
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
